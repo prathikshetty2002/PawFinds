6739c2ba64b3a5853cfba14a41a4c37b10108165
Prathik Shetty <63772910+prathikshetty2002@users.noreply.github.com>
2025-03-24 23:13:01 +0530

    Update AdoptForm.js

--- Client/src/Components/AdoptForm/AdoptForm.js
+++ Client/src/Components/AdoptForm/AdoptForm.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 import React, { useState } from "react";
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://pawfinds.onrender.com';
 function AdoptForm(props) {
