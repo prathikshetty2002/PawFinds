41ca3c225ab7cd8b2cdb4e1bc97445d486288f19
Prathik Shetty <63772910+prathikshetty2002@users.noreply.github.com>
2025-03-24 22:46:40 +0530

    Add files via upload

---
+++ Client/src/Components/AdminPanel/AdoptedHistory.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import AdoptedCards from './AdoptedCards';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://pawfinds.onrender.com';
+const AdoptedHistory = () => {
+  const [requests, setRequests] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchAdoptedPets = async () => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/adoptedPets`);
+      if (!response.ok) {
+        throw new Error('An error occurred while fetching adopted pets');
+      }
+      const data = await response.json();
+      setRequests(data);
+    } catch (error) {
+      console.error('Error fetching adopted pets:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchAdoptedPets();
+  }, []);
+
+  return (
+    <div className='pet-container'>
+      {loading ? (
+        <p>Loading...</p>
+      ) : 
+      requests.length > 0 ? (
+        requests.map((request) => (
+          <AdoptedCards
+            key={request._id}
+            pet={request}
+            updateCards={fetchAdoptedPets}
+            deleteBtnText="Delete History"
+            approveBtn={false}
+          />
+        ))
+      ) : (
+        <p>No Adopted Pets available</p>
+      )}
+    </div>
+  );
+};
+
+export default AdoptedHistory;
