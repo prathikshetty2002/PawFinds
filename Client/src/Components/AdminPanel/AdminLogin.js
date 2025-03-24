440637797d12f61bfc4286fea70ab9a176a5823a
Prathik Shetty <63772910+prathikshetty2002@users.noreply.github.com>
2025-03-24 23:07:27 +0530

    Update AdminLogin.js

--- Client/src/Components/AdminPanel/AdminLogin.js
+++ Client/src/Components/AdminPanel/AdminLogin.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 import React, { useState, useEffect } from "react";
 import AdminPanel from "./AdminPanel";
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://pawfinds.onrender.com';
